cbb4cdc72ca83089853a3609a68384e59b8aa508
grifocx <ergriffin@gmail.com>
2025-05-14 12:53:23 -0400

    text tweaks

--- src/components/ServicesList.tsx
+++ src/components/ServicesList.tsx
@@ -71,7 +71,7 @@ const services = [
 export function ServicesList() {
   return (
     <div>
-      <h2 className="text-2xl font-bold mb-6">Affordable Bicycle Services in Haymarket, VA</h2>
+      <h2 className="text-2xl font-bold mb-6">Quality and Affordable Bicycle Services in Haymarket, VA</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mb-24">
         {services.map((service) => (
           <Card key={service.title} className="hover-scale">
